c505efb0357a7bb8ab2921af1a147003d0ed6240
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2021-10-06 17:05:54 +0700

    add testi section

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+let menuOpen = document.querySelector('.menu-toggle');
+let menuWrapper = document.querySelector('.menu-wrapper');
+
+menuOpen.addEventListener('click', function () {
+    menuOpen.classList.toggle('bx-x');
+    menuWrapper.classList.toggle('active')
+})
+
+
+var swiper = new Swiper(".mySwiper", {
+    grabCursor: true,
+    centeredSlides: false,
+    spaceBetween: 30,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 0,
+        modifier: 1,
+        slideShadows: true,
+    },
+    navigation: {
+        nextEl: ".bxs-chevron-right-circle",
+        prevEl: ".bxs-chevron-left-circle",
+    },
+});
